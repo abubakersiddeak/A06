c28879e3a34dfa9d3f9baf88524572a634d66e4d
Your Name <abubakersiddeak@gmail.com>
2025-09-07 11:02:29 +0600

    add mobile meny troggule button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+//  mobile menu troggule button
+const menuModal = document.getElementById("menuModal");
+let num = 1;
+function handleManuClick() {
+  num++;
+  if (num % 2 === 0) {
+    menuModal.classList.remove("hidden");
+    menuModal.classList.add("flex");
+  } else {
+    menuModal.classList.remove("flex");
+    menuModal.classList.add("hidden");
+  }
+}
